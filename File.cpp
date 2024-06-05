8e630e8730a86f2cd7933aa22ae2066eea67a497
pkisensee <pkisensee@msn.com>
2024-06-05 16:24:14 -0700

    Remove old files. Compile clean at max warnings.

--- File.cpp
+++ File.cpp
@@ -11,16 +11,18 @@
 // 
 //  This software is provided "as is" and without any express or implied
 //  warranties.
+// 
+//  *** Microsoft Windows implementation ***
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// Includes
 #include <cassert>
 #include <limits>
 #include "File.h"
 #include "Log.h"
 #include "StrUtil.h"
 
+// Windows headers
 #define NOMINMAX 1
 #include "Windows.h"
 #include "ShellAPI.h"
@@ -33,36 +35,36 @@ namespace { // anonymous namespace
 
 struct CreateFileParams
 {
-    uint32_t access = 0u;
-    uint32_t share = 0u;
-    uint32_t create = 0u;
-    uint32_t attribs = FILE_ATTRIBUTE_NORMAL;
+  uint32_t access = 0u;
+  uint32_t share = 0u;
+  uint32_t create = 0u;
+  uint32_t attribs = FILE_ATTRIBUTE_NORMAL;
 
-    CreateFileParams( uint32_t howToOpen, FileFlags flags )
-      : create( howToOpen )
-    {
-        // Can't specify both sequential and random
-        assert( !( ( flags & FileFlags::SequentialScan ) && ( flags & FileFlags::RandomAccess ) ) );
+  CreateFileParams( uint32_t howToOpen, FileFlags flags )
+    : create( howToOpen )
+  {
+    // Can't specify both sequential and random
+    assert( !( ( flags & FileFlags::SequentialScan ) && ( flags & FileFlags::RandomAccess ) ) );
 
-        // Need to open for either reading and/or writing
-        assert( ( flags & FileFlags::Read ) || ( flags & FileFlags::Write ) );
+    // Need to open for either reading and/or writing
+    assert( ( flags & FileFlags::Read ) || ( flags & FileFlags::Write ) );
 
-        if( flags & FileFlags::Read )           access |= GENERIC_READ;
-        if( flags & FileFlags::Write )          access |= GENERIC_WRITE;
+    if( flags & FileFlags::Read )       access |= GENERIC_READ;
+    if( flags & FileFlags::Write )      access |= GENERIC_WRITE;
 
-        if( flags & FileFlags::ShareRead )      share |= FILE_SHARE_READ;
-        if( flags & FileFlags::ShareWrite )     share |= FILE_SHARE_WRITE;
-        if( flags & FileFlags::ShareDelete )    share |= FILE_SHARE_DELETE;
+    if( flags & FileFlags::SharedRead )    share |= FILE_SHARE_READ;
+    if( flags & FileFlags::SharedWrite )   share |= FILE_SHARE_WRITE;
+    if( flags & FileFlags::SharedDelete )  share |= FILE_SHARE_DELETE;
 
-        if( flags & FileFlags::SequentialScan ) attribs |= FILE_FLAG_SEQUENTIAL_SCAN;
-        if( flags & FileFlags::RandomAccess )   attribs |= FILE_FLAG_RANDOM_ACCESS;
-    }
+    if( flags & FileFlags::SequentialScan ) attribs |= FILE_FLAG_SEQUENTIAL_SCAN;
+    if( flags & FileFlags::RandomAccess )   attribs |= FILE_FLAG_RANDOM_ACCESS;
+  }
 };
 
 uint64_t FiletimeToU64( const FILETIME& ft )
 {
-    ULARGE_INTEGER large = { ft.dwLowDateTime, ft.dwHighDateTime };
-    return large.QuadPart;
+  ULARGE_INTEGER large = { { ft.dwLowDateTime, ft.dwHighDateTime } };
+  return large.QuadPart;
 }
 
 } // end anonymous namespace
@@ -72,16 +74,16 @@ uint64_t FiletimeToU64( const FILETIME& ft )
 // Ctors
 
 File::File() :
-    mPath(),
-    mFile( INVALID_HANDLE_VALUE )
+  path_(),
+  file_( INVALID_HANDLE_VALUE )
 {
 }
 
 File::File( const fs::path& path ) :
-    mPath( path ),
-    mFile( INVALID_HANDLE_VALUE )
+  path_( path ),
+  file_( INVALID_HANDLE_VALUE )
 {
-    mPath.make_preferred(); // ensure Windows separators
+  path_.make_preferred(); // ensure Windows separators
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -90,100 +92,94 @@ File::File( const fs::path& path ) :
 
 File::~File()
 {
-    if( mFile != INVALID_HANDLE_VALUE )
-        ::CloseHandle( mFile );
+  if( file_ != INVALID_HANDLE_VALUE )
+    ::CloseHandle( file_ );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
 // Create the file or directory, including any intermediate directories.
-// Closes any currently open file or folder
+// Closes any currently open file or folder.
 
 bool File::Create( FileFlags flags )
 {
-    CreateFileParams cfp( CREATE_ALWAYS, flags );
-    assert( mPath.has_filename() || mPath.has_parent_path() );
-    Close();
-    if( mPath.has_parent_path() )
+  CreateFileParams cfp( CREATE_ALWAYS, flags );
+  assert( path_.has_filename() || path_.has_parent_path() );
+  Close();
+  if( path_.has_parent_path() )
+  {
+    fs::create_directories( path_.parent_path() );
+    if( !path_.has_filename() )
     {
-        fs::create_directories( mPath.parent_path() );
-        if( !mPath.has_filename() )
-        {
-            // If a directory was specified, we need to adjust some flags
-            cfp.attribs |= FILE_FLAG_BACKUP_SEMANTICS;
-            cfp.create = OPEN_EXISTING;
-        }
+      // If directory specified, adjust flags
+      cfp.attribs |= FILE_FLAG_BACKUP_SEMANTICS;
+      cfp.create = OPEN_EXISTING;
     }
+  }
 
-    mFile = ::CreateFileW( mPath.c_str(), cfp.access, cfp.share, NULL, cfp.create, cfp.attribs, NULL );
-    if( mFile == INVALID_HANDLE_VALUE )
-        PKLOG_WARN( "CreateFileW failed to create file %S with error %d\n", mPath.c_str(), ::GetLastError() );
-    return IsOpen();
+  file_ = ::CreateFileW( path_.c_str(), cfp.access, cfp.share, NULL, cfp.create, cfp.attribs, NULL );
+  if( file_ == INVALID_HANDLE_VALUE )
+    PKLOG_WARN( "CreateFileW failed to create file %S with error %d\n", 
+                path_.c_str(), ::GetLastError() );
+  return IsOpen();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Open the file or directory
-// Closes any currently open file
+// Open the file or directory. Closes any currently open file.
 
 bool File::Open( FileFlags flags )
 {
-    CreateFileParams cfp( OPEN_EXISTING, flags );
-    assert( mPath.has_filename() || mPath.has_parent_path() );
+  CreateFileParams cfp( OPEN_EXISTING, flags );
+  assert( path_.has_filename() || path_.has_parent_path() );
 
-    // Required to open a directory
-    if( !mPath.has_filename() )
-        cfp.attribs |= FILE_FLAG_BACKUP_SEMANTICS;
+  // Required to open a directory
+  if( !path_.has_filename() )
+    cfp.attribs |= FILE_FLAG_BACKUP_SEMANTICS;
 
-    Close();
-    mFile = ::CreateFileW( mPath.c_str(), cfp.access, cfp.share, NULL, cfp.create, cfp.attribs, NULL );
-    if( mFile == INVALID_HANDLE_VALUE )
-        PKLOG_WARN( "CreateFileW failed to open file %S with error %d\n", mPath.c_str(), ::GetLastError() );
-    return IsOpen();
+  Close();
+  file_ = ::CreateFileW( path_.c_str(), cfp.access, cfp.share, NULL, cfp.create, cfp.attribs, NULL );
+  if( file_ == INVALID_HANDLE_VALUE )
+    PKLOG_WARN( "CreateFileW failed to open file %S with error %d\n", 
+                path_.c_str(), ::GetLastError() );
+  return IsOpen();
 }
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// Close the file or directory
 
 void File::Close()
 {
-    if( mFile != INVALID_HANDLE_VALUE )
-    {
-        ::CloseHandle( mFile );
-        mFile = INVALID_HANDLE_VALUE;
-    }        
+  if( file_ != INVALID_HANDLE_VALUE )
+  {
+    ::CloseHandle( file_ );
+    file_ = INVALID_HANDLE_VALUE;
+  }    
 }
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// Report if file is open
 
 bool File::IsOpen() const
 {
-    return( mFile != INVALID_HANDLE_VALUE );
+  return( file_ != INVALID_HANDLE_VALUE );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// The size of the file or folder in bytes
+// Size of file/folder in bytes
 
-size_t File::GetLength() const
+uint64_t File::GetLength() const
 {
-    if( !IsOpen() )
-    {
-        WIN32_FILE_ATTRIBUTE_DATA fa = { 0 };
-        ::GetFileAttributesExW( mPath.c_str(), GetFileExInfoStandard, &fa );
-        return fa.nFileSizeLow + ( uint64_t( fa.nFileSizeHigh ) << 32 );
-    }
-    FILE_STANDARD_INFO FileInfo = { 0 };
-    BOOL bSuccess = ::GetFileInformationByHandleEx( mFile, FileStandardInfo, 
-                                                    &FileInfo, sizeof(FileInfo) );
-    assert( bSuccess );
-    assert( FileInfo.EndOfFile.QuadPart >= 0 );
-    size_t len = static_cast<size_t>( FileInfo.EndOfFile.QuadPart );
-    assert( len <= std::numeric_limits<size_t>::max() );
-    return len;
+  if( !IsOpen() )
+  {
+    WIN32_FILE_ATTRIBUTE_DATA fa = { 0 };
+    ::GetFileAttributesExW( path_.c_str(), GetFileExInfoStandard, &fa );
+    uint64_t lo = fa.nFileSizeLow;
+    uint64_t hi = fa.nFileSizeHigh;
+    return lo + ( hi << 32 );
+  }
+  FILE_STANDARD_INFO FileInfo = { { { 0 } } };
+  [[maybe_unused]] auto success = ::GetFileInformationByHandleEx( file_, 
+                                      FileStandardInfo, &FileInfo, sizeof(FileInfo) );
+  assert( success );
+  assert( FileInfo.EndOfFile.QuadPart >= 0 );
+  uint64_t len = static_cast<uint64_t>( FileInfo.EndOfFile.QuadPart );
+  return len;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -192,41 +188,45 @@ size_t File::GetLength() const
 
 bool File::GetFileTimes( File::Times& fileTimes ) const
 {
-    if( !IsOpen() )
-    {
-        WIN32_FIND_DATAW fd = { 0 };
-        HANDLE hFile = ::FindFirstFileW( mPath.c_str(), &fd );
-        assert( hFile != INVALID_HANDLE_VALUE );
-        if( hFile == INVALID_HANDLE_VALUE )
-            return false;
-        fileTimes.creationTime   = FiletimeToU64( fd.ftCreationTime );
-        fileTimes.lastAccessTime = FiletimeToU64( fd.ftLastAccessTime );
-        fileTimes.lastWriteTime  = FiletimeToU64( fd.ftLastWriteTime );
-        FindClose( hFile );
-        return true;
-    }
+  if( !IsOpen() )
+  {
+    WIN32_FIND_DATAW fd = { 0 };
+    auto hFile = ::FindFirstFileW( path_.c_str(), &fd );
+    assert( hFile != INVALID_HANDLE_VALUE );
+    if( hFile == INVALID_HANDLE_VALUE )
+      return false;
 
-    FILETIME creationTime, lastAccessTime, lastWriteTime;
-    BOOL bSuccess = ::GetFileTime( mFile, &creationTime, &lastAccessTime, &lastWriteTime );
-    assert( bSuccess );
-    fileTimes.creationTime   = FiletimeToU64( creationTime );
-    fileTimes.lastAccessTime = FiletimeToU64( lastAccessTime );
-    fileTimes.lastWriteTime  = FiletimeToU64( lastWriteTime );
+    // TODO use std::chrono times
+    fileTimes.creationTime   = FiletimeToU64( fd.ftCreationTime );
+    fileTimes.lastAccessTime = FiletimeToU64( fd.ftLastAccessTime );
+    fileTimes.lastWriteTime  = FiletimeToU64( fd.ftLastWriteTime );
+    ::FindClose( hFile );
     return true;
+  }
+
+  FILETIME creationTime, lastAccessTime, lastWriteTime;
+  [[maybe_unused]] auto success = ::GetFileTime( file_, 
+                                     &creationTime, &lastAccessTime, &lastWriteTime );
+  assert( success );
+  fileTimes.creationTime   = FiletimeToU64( creationTime );
+  fileTimes.lastAccessTime = FiletimeToU64( lastAccessTime );
+  fileTimes.lastWriteTime  = FiletimeToU64( lastWriteTime );
+  return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Set the next position for reading or writing
+// Set the next position for reading or writing. Position is always from the
+// beginning of the file.
 
-bool File::SetPos( size_t pos ) const
+bool File::SetPos( uint64_t pos ) const
 {
-    assert( IsOpen() );
-    assert( pos <= static_cast<size_t>(std::numeric_limits<long long>::max()) );
-    LARGE_INTEGER largePos = { .QuadPart = static_cast<long long>(pos) };
+  assert( IsOpen() );
 
-    auto success = ::SetFilePointerEx( mFile, largePos, NULL, FILE_BEGIN );
-    return success;
+  // With FILE_BEGIN, largePos is interpreted as unsigned, so cast is safe
+  LARGE_INTEGER largePos = { .QuadPart = static_cast<LONGLONG>(pos) };
+  auto success = ::SetFilePointerEx( file_, largePos, NULL, FILE_BEGIN );
+  return success == TRUE;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -235,30 +235,30 @@ bool File::SetPos( size_t pos ) const
 
 bool File::Read( void* pBuffer, size_t bytes ) const
 {
-    size_t bytesRead;
-    bool success = Read( pBuffer, bytes, bytesRead );
-    return ( success && ( bytes == bytesRead ) );
+  size_t bytesRead = 0;
+  auto success = Read( pBuffer, bytes, bytesRead );
+  return ( success && ( bytes == bytesRead ) );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Read a buffer from the current file position; return bytes read
+// Read a buffer from the current file position; returns bytesRead
 
 bool File::Read( void* pBuffer, size_t bytes, size_t& bytesRead ) const
 {
-    assert( mPath.has_filename() );
-    assert( IsOpen() );
-    assert( pBuffer != nullptr );
-    assert( bytes > 0 );
+  assert( path_.has_filename() );
+  assert( IsOpen() );
+  assert( pBuffer != nullptr );
+  assert( bytes > 0 );
 
-    // If this fails, we need to adjust the function to be a loop
-    assert( bytes <= std::numeric_limits<DWORD>::max() );
-    DWORD bytes32 = static_cast<DWORD>( bytes );
+  // If this fails, adjust the function to be a loop
+  assert( bytes <= std::numeric_limits<DWORD>::max() );
+  DWORD bytes32 = static_cast<DWORD>( bytes );
 
-    DWORD br = 0u;
-    BOOL bSuccess = ::ReadFile( mFile, pBuffer, bytes32, &br, NULL );
-    bytesRead = br;
-    return( bSuccess == TRUE );
+  DWORD br = 0u;
+  auto success = ::ReadFile( file_, pBuffer, bytes32, &br, NULL );
+  bytesRead = br;
+  return( success == TRUE );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -267,28 +267,26 @@ bool File::Read( void* pBuffer, size_t bytes, size_t& bytesRead ) const
 
 bool File::Write( const void* pBuffer, size_t bytes )
 {
-    assert( mPath.has_filename() );
-    assert( IsOpen() );
-    assert( pBuffer != nullptr );
-    assert( bytes > 0 );
-    assert( bytes <= std::numeric_limits<DWORD>::max() );
-    DWORD bytes32 = static_cast<DWORD>( bytes );
-    DWORD bytesWritten = 0;
-    BOOL bSuccess = ::WriteFile( mFile, pBuffer, bytes32, &bytesWritten, NULL );
-    return( ( bSuccess == TRUE ) && ( bytes == bytesWritten ) );
+  assert( path_.has_filename() );
+  assert( IsOpen() );
+  assert( pBuffer != nullptr );
+  assert( bytes <= std::numeric_limits<DWORD>::max() );
+  DWORD bytes32 = static_cast<DWORD>( bytes );
+  DWORD bytesWritten = 0;
+  auto success = ::WriteFile( file_, pBuffer, bytes32, &bytesWritten, NULL );
+  return( success && ( bytes == bytesWritten ) );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Flush file to disk. Does nothing for directories or files opened only for reading
+// Flush file to storage medium. Does nothing for directories or read-only files
 
-void File::Flush()
+void File::Flush() const
 {
-    assert( mPath.has_filename() );
-    assert( IsOpen() );
-    BOOL bSuccess = ::FlushFileBuffers( mFile );
-    assert( bSuccess );
-    (void)bSuccess;
+  assert( path_.has_filename() );
+  assert( IsOpen() );
+  [[maybe_unused]] BOOL success = ::FlushFileBuffers( file_ );
+  assert( success );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -296,26 +294,27 @@ void File::Flush()
 // Delete the file or directory. Requires that the file is closed and not
 // read-only. Allows the file to be moved to the recycle bin, if one exists.
 
-bool File::Delete( bool bRecycle ) const
+bool File::Delete( bool recycle ) const
 {
-    assert( !IsOpen() );
-    if( IsOpen() )
-        return false;
+  assert( !IsOpen() );
+  if( IsOpen() )
+    return false;
 
-    SHFILEOPSTRUCTW FileOp = { 0 };
-    FileOp.wFunc = FO_DELETE;
-    FileOp.fFlags = ( bRecycle ? FOF_ALLOWUNDO : 0 ) | FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT;
+  SHFILEOPSTRUCTW FileOp = { 0 };
+  FileOp.wFunc = FO_DELETE;
+  FileOp.fFlags = static_cast<FILEOP_FLAGS>( recycle ? FOF_ALLOWUNDO : 0x0 );
+  FileOp.fFlags |= FOF_NOCONFIRMATION | FOF_NOERRORUI | FOF_SILENT;
 
-    // Path must be fully qualfied, hence fs::absolute
-    // Directories must have any trailing slashes removed
-    // String is required to end in two null characters
-    std::wstring fullPath = fs::absolute(mPath).wstring();
-    StrUtilT<wchar_t>::ToTrimmedTrailing( fullPath, L"\\/" );
-    fullPath += L'\0';
-    FileOp.pFrom = fullPath.c_str();
-        
-    auto result = ::SHFileOperationW( &FileOp );
-    return( result == 0 );
+  // Path must be fully qualfied, hence fs::absolute.
+  // Directories must have any trailing slashes removed.
+  // String is required to end in two null characters (!)
+  std::wstring fullPath = fs::absolute(path_).wstring();
+  StrUtilT<wchar_t>::ToTrimmedTrailing( fullPath, L"\\/" );
+  fullPath += L'\0';
+  FileOp.pFrom = fullPath.c_str();
+    
+  auto result = ::SHFileOperationW( &FileOp );
+  return( result == 0 );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- File.h
+++ File.h
@@ -11,6 +11,11 @@
 // 
 //  This software is provided "as is" and without any express or implied
 //  warranties.
+// 
+//-----------------------------------------------------------------------------
+// 
+//  Fast file reading and writing. Typically the implementation is native code
+//  for maximum speed and performance.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -26,25 +31,25 @@ namespace PKIsensee
 
 enum class FileFlags : uint32_t
 {
-    Write          = 1 << 0,
-    Read           = 1 << 1,
-    ShareRead      = 1 << 2,
-    ShareWrite     = 1 << 3,
-    ShareDelete    = 1 << 4,
-    SequentialScan = 1 << 5,
-    RandomAccess   = 1 << 6,
+  Write          = 1 << 0,
+  Read           = 1 << 1,
+  SharedRead     = 1 << 2,
+  SharedWrite    = 1 << 3,
+  SharedDelete   = 1 << 4,
+  SequentialScan = 1 << 5,
+  RandomAccess   = 1 << 6,
 };
 
 constexpr FileFlags operator | ( FileFlags lhs, FileFlags rhs )
 {
-    using T = typename std::underlying_type<FileFlags>::type;
-    return static_cast<FileFlags>( static_cast<T>( lhs ) | static_cast<T>( rhs ) );
+  using T = typename std::underlying_type<FileFlags>::type;
+  return static_cast<FileFlags>( static_cast<T>( lhs ) | static_cast<T>( rhs ) );
 }
 
 constexpr bool operator & ( FileFlags lhs, FileFlags rhs )
 {
-    using T = typename std::underlying_type<FileFlags>::type;
-    return static_cast<bool>( static_cast<T>( lhs ) & static_cast<T>( rhs ) );
+  using T = typename std::underlying_type<FileFlags>::type;
+  return static_cast<bool>( static_cast<T>( lhs ) & static_cast<T>( rhs ) );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -53,79 +58,85 @@ class File
 {
 public:
 
-    struct Times
-    {
-        // 100-nanosec intervals since 1601/01/01Z
-        uint64_t creationTime;
-        uint64_t lastAccessTime;
-        uint64_t lastWriteTime;
-    };
+  struct Times
+  {
+    // 100-nanosec intervals since 1601/01/01Z
+    uint64_t creationTime;
+    uint64_t lastAccessTime;
+    uint64_t lastWriteTime;
+  };
 
 public:
 
-    File();
-    explicit File( const std::filesystem::path& );
-    ~File();
+  File();
+  explicit File( const std::filesystem::path& );
+  ~File();
 
-    // Disable copy/move
-    File( const File& ) = delete;
-    File& operator=( const File& ) = delete;
-    File( File&& ) = delete;
-    File& operator=( File&& ) = delete;
+  // Disable copy/move
+  File( const File& ) = delete;
+  File& operator=( const File& ) = delete;
+  File( File&& ) = delete;
+  File& operator=( File&& ) = delete;
 
-    bool Create( FileFlags fileFlags );
-    bool Open( FileFlags fileFlags );
-    void Close();
-    bool IsOpen() const;
-    
-    size_t GetLength() const;
-    bool GetFileTimes( File::Times& ) const;
-    bool Read( void*, size_t ) const;
-    bool Read( void*, size_t, size_t& ) const;
-    bool SetPos( size_t ) const;
-    bool Write( const void*, size_t );
-    void Flush();
-    
-    bool Delete( bool bRecycle = true ) const;
+  bool Create( FileFlags fileFlags );
+  bool Open( FileFlags fileFlags );
+  void Close();
+  bool IsOpen() const;
+  
+  uint64_t GetLength() const;
+  bool GetFileTimes( File::Times& ) const;
+  bool Read( void*, size_t ) const;
+  bool Read( void*, size_t, size_t& ) const;
+  bool SetPos( uint64_t ) const;
+  bool Write( const void*, size_t );
+  void Flush() const;
+  
+  bool Delete( bool bRecycle = true ) const;
 
-    void SetFile( const std::filesystem::path& path ) {
-        Close();
-        mPath = path;
-    }
-    std::filesystem::path GetPath() const {
-        return mPath;
-    }
+  void SetFile( const std::filesystem::path& path ) {
+    Close();
+    path_ = path;
+  }
 
-    ///////////////////////////////////////////////////////////////////////////////
-    //
-    // Open, read entire file into memory, and close. T must support resize() and
-    // data() functions, e.g. string, vector, etc.
+  std::filesystem::path GetPath() const {
+    return path_;
+  }
 
-    template <class T>
-    static bool ReadEntireFile( const std::filesystem::path& path, T& result )
+  ///////////////////////////////////////////////////////////////////////////////
+  //
+  // Open, read entire file into memory, and close. T must support resize() and
+  // data() functions, e.g. string, vector, etc. Returns false if file can't be
+  // opened or insufficient memory. TODO enable concepts
+
+  template <class T>
+  static bool ReadEntireFile( const std::filesystem::path& path, T& result )
+  {
+    // Open with least restrictions
+    File f( path );
+    if( !f.Open( FileFlags::Read | FileFlags::SharedRead | FileFlags::SequentialScan ) )
+      return false;
+
+    // Create buffer the size of the file
+    auto len = f.GetLength();
+    try { result.resize( len ); }
+    catch( std::bad_alloc& )
     {
-        // Open with least restrictions
-        File f( path );
-        if( !f.Open( FileFlags::Read | FileFlags::ShareRead | FileFlags::SequentialScan ) )
-            return false;
-
-        // Create buffer the size of the file
-        auto len = f.GetLength();
-        result.resize( len );
-
-        // Read file into memory
-        if( !f.Read( result.data(), len ) )
-        {
-            result.resize( 0 );
-            return false;
-        }
-        return true;
+      return false;
     }
+
+    // Read file into memory
+    if( !f.Read( result.data(), len ) )
+    {
+      result.resize( 0 );
+      return false;
+    }
+    return true;
+  }
 
 private:
 
-    std::filesystem::path mPath;
-    void*                 mFile;
+  std::filesystem::path path_;
+  void*                 file_;
 
 }; // class File
 
